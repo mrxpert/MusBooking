fbe21373c620fd2fe07f26a07aee2468f411e2bc
Vladislav Balyukh <vladislav.balyukh@balance-pl.ru>
2025-03-10 16:50:44 +0300

    adds date locator & click on it

--- tests/booking.spec.ts
+++ tests/booking.spec.ts
@@ -1,9 +1,15 @@
 import { test, Page, expect } from '@playwright/test';
 
 test('book_appointment', async ({ page }) => {
-
+    
     const url = 'https://musbooking.com/katalog/repetitsionnye-bazy/moskva-i-mo/repetitsionnaya-baza-proletarskaya-krestyanskaya-zastava-hendrix-studio-proletarskaya-i-vintage';
-    const date = '//div[@id="{formatted_date}-23"]';
+ 
+    const datePrettyfier = (date: Date) : string => (date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString() + '-' + date.getDate().toString());
+    
+    const currentDate = new Date();
+    const thirtyDays = 30 * 24 * 60 * 60 * 1000;
+    const monthInAdvance = new Date(currentDate.getTime() + thirtyDays);
+    const dateTimeLocator: string = `//div[@id="${datePrettyfier(monthInAdvance)}-23"]`; // refactor it
     
     await page.goto(url, { timeout: 50000 });
 
@@ -16,6 +22,9 @@ test('book_appointment', async ({ page }) => {
     // Ожидание iframe со слотами
     await page.waitForSelector('iframe');
     const frame = page.frameLocator('iframe');
+
+    // Нажатие на слот в таблице
+    await frame.locator(dateTimeLocator).click({ timeout: 50_000 });
 
     await expect(page).toHaveTitle('MUSbooking');
 })
